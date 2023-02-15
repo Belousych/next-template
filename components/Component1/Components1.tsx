c0c19101fd1636ec5232f4471baed83ccfec0ad8
Belousych <wasik787@gmail.com>
2023-02-15 16:59:36 +0500

    first commit

---
+++ components/Component1/Components1.tsx
@@ -0,0 +1,11 @@
+import React, { PureComponent } from "react";
+
+type Props = {};
+
+class Components1 extends PureComponent<Props> {
+  render() {
+    return <div>Components1</div>;
+  }
+}
+
+export default Components1;
